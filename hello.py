308be5a1f3389e7c2306ad89e6bd776c2152f14e
Tero Ykspetäjä <tero@valohai.com>
2024-06-20 14:20:31 +0300

    🏗️ Move input handling to a separate script
    Allows still testing the environment with ultra-simple hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
-import valohai
-
-valohai.prepare(step="hello-job")
+"""Simple hello world example.
+Does nothing except demonstrate that a script can be run.
+"""
 
 print("Hello, world!")
-
---
+++ list-inputs.py
@@ -0,0 +1,16 @@
+"""List all inputs given to the current step."""
+
+import valohai
+
+valohai.prepare(step="list-inputs")
+
+print("I have inputs!")
+
+inputs = {
+    "dataset": "",
+}
+
+valohai.prepare(step="list-inputs", default_inputs=inputs)
+
+for file_path in valohai.inputs("dataset").paths():
+    print("{file_path=}")
